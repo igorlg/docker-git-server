9a932321d760674e78c001b38116a175e40bad9b
Igor Gentil <igor.lg@gmail.com>
2020-12-30 11:47:44 +1100

    Updated Check script

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,20 +2,14 @@
 
 source ./scripts/vars.sh
 
-[[ -f "$VOLUME_CONFIG_PATH" ]] && \
-[[ -f "$VOLUME_KEYS_PATH" ]]   && \
-[[ -d "$VOLUME_DATA_PATH" ]]   && \
-[[ `stat -c '%a' $VOLUME_KEYS_PATH` == "600" ]]
-
-#!/bin/ash
-
-source ./scripts/vars.sh
-
-[[ -z "$IMAGE_PREFIX" ]]	&& \
-[[ -z "$IMAGE_NAME" ]]		&& \
-[[ -z "$IMAGE_VERSION" ]]	&& \
-[[ -z "$CONTAINER_NAME" ]]	&& \
-[[ -z "$VOLUME_DATA_PATH" ]]
-
+# [[ -f "$VOLUME_CONFIG_PATH" ]] && \
+[[ -f "$VOLUME_KEYS_PATH" ]]                    && \
+[[ -d "$VOLUME_DATA_PATH" ]]                    && \
+[[ `stat -c '%a' $VOLUME_KEYS_PATH` == "600" ]] && \
+[[ -z "$IMAGE_PREFIX" ]]	                      && \
+[[ -z "$IMAGE_NAME" ]]		                      && \
+[[ -z "$IMAGE_VERSION" ]]	                      && \
+[[ -z "$CONTAINER_NAME" ]]	                    && \
+[[ -z "$VOLUME_DATA_PATH" ]]                    && \
 [[ -d "$VOLUME_DATA_PATH" ]]
 
